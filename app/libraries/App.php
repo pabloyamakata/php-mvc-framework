d7baf6e89fb6666c684e132cc0cabd98c8ecd057
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-25 20:07:59 -0300

    docs: remove comments

--- app/libraries/App.php
+++ app/libraries/App.php
@@ -22,8 +22,6 @@
                 }
             }
 
-            // Requires a specific controller and instantiates it
-
             require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
 
             $this->controller = new $this->controller;
