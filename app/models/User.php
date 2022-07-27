56652228bc964ecb431afe085171f7affb1d5912
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-26 21:15:41 -0300

    feat: add methods to access users table

--- app/models/User.php
+++ app/models/User.php
@@ -1,6 +1,27 @@
 <?php
 
-    class User
+    class User extends Connection
     {
-        public $name;
+        public function create($firstName, $lastName, $email)
+        {
+            $sql = "INSERT INTO users(first_name, last_name, email) VALUES (?, ?, ?)";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute([$firstName, $lastName, $email]);
+        }
+
+        public function find($id)
+        {
+            $sql = "SELECT * FROM users WHERE id = ?";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute([$id]);
+            $result = $stmt->fetchAll();
+            return $result;
+        }
+
+        public function findAll()
+        {
+            $sql = "SELECT * FROM users";
+            $result = $this->connect()->query($sql);
+            return $result;
+        }
     }
