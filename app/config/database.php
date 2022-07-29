bbe9cac82650444c441f663d152917c82edf9c55
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-29 17:35:16 -0300

    refactor: change value of constant DB_NAME

--- app/config/database.php
+++ app/config/database.php
@@ -1,10 +1,6 @@
 <?php
 
-    // Returns the name of the project main directory or root directory
-    $pathArray = explode('/', str_replace('\\', '/', dirname(dirname(__DIR__))));
-    $rootDirectory = end($pathArray);
-
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', $rootDirectory);
+    define('DB_NAME', 'websavant');
