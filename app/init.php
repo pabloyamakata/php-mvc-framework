c77ac07805256fa4b49c9a2288c5c84ca4b65c4e
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-31 17:38:25 -0300

    refactor: use autoloader to load classes
    Require also config files to make its constants available in all scripts

--- app/init.php
+++ app/init.php
@@ -1,5 +1,6 @@
 <?php
 
-    require_once 'libraries/App.php';
-    require_once 'libraries/Connection.php';
-    require_once 'libraries/Controller.php';
+    require_once 'config/database.php';
+    require_once 'config/paths.php';
+
+    spl_autoload_register(fn($className) => require_once 'libraries/' . $className . '.php');
