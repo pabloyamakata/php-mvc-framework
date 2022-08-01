3e2862d71166cc5b47429f6cf0630123045a060b
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-07-31 21:45:37 -0300

    Include header and footer

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    <h1>The PHP Framework for Small Enterprises</h1>
-</body>
-</html>
+<?php include_once ROOT_PATH . 'app/views/inc/header.php'; ?>
+<?php include_once ROOT_PATH . 'app/views/inc/footer.php'; ?>
