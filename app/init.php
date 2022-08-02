fb6e8ef84ed48565ca4046db07b4df7e50ec5ff5
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-02 02:09:10 -0300

    refactor: change import path

--- app/init.php
+++ app/init.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once 'config/database.php';
-    require_once 'config/paths.php';
+    require_once 'config/location.php';
     require_once 'helpers/redirect.php';
 
     spl_autoload_register(fn($className) => require_once 'libraries/' . $className . '.php');
