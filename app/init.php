d69715339eb07970d2d100ffaaa56adf0916697c
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-02 02:14:29 -0300

    refactor: change argument name

--- app/init.php
+++ app/init.php
@@ -4,4 +4,4 @@
     require_once 'config/location.php';
     require_once 'helpers/redirect.php';
 
-    spl_autoload_register(fn($className) => require_once 'libraries/' . $className . '.php');
+    spl_autoload_register(fn($class) => require_once 'libraries/' . $class . '.php');
