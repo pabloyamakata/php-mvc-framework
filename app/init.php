bb4d95dd0513ff33ea0d8254e435df3d1436f36f
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-01 21:18:07 -0300

    feat: require helper function

--- app/init.php
+++ app/init.php
@@ -2,5 +2,6 @@
 
     require_once 'config/database.php';
     require_once 'config/paths.php';
+    require_once 'helpers/redirect.php';
 
     spl_autoload_register(fn($className) => require_once 'libraries/' . $className . '.php');
