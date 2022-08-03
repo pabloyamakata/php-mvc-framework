b4660f27cb9eb76446b664b1a0995ac82a77117b
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-02 23:36:14 -0300

    feat: add buttons to edit and delete a record from database

--- app/views/users/show.php
+++ app/views/users/show.php
@@ -3,5 +3,9 @@
 <h2><?= $user['firstname'] . ' ' . $user['lastname'] ?></h2>
 <p>Email: <?= $user['email'] ?></p>
 <p>Age: <?= $user['age'] ?></p>
+<p><a href=<?= DOMAIN . 'usercontroller/edit/' . $user['id'] ?>>Edit</a></p>
+<form action=<?= DOMAIN . 'usercontroller/destroy/' . $user['id'] ?> method="POST">
+    <input type="submit" value="Delete">
+</form>
 
 <?php include_once ROOT_PATH . 'app/views/inc/footer.php'; ?>
