6a448414cb3edbb7a6a0b093884522f798c4c168
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-03 18:50:45 -0300

    refactor: use Config static functions

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WebSavant</title>
     <!-- Include styles.css -->
-    <link rel="stylesheet" href=<?= DOMAIN . 'css/styles.css' ?>>
+    <link rel="stylesheet" href=<?= Config::getDomain() . 'css/styles.css' ?>>
 </head>
 <body>
