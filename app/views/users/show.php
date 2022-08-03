b02e6a3bf286aff4b5e47c3044daf27e3963de60
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-03 00:05:20 -0300

    style: add blank lines

--- app/views/users/show.php
+++ app/views/users/show.php
@@ -1,9 +1,12 @@
 <?php include_once ROOT_PATH . 'app/views/inc/header.php'; ?>
 
 <h2><?= $user['firstname'] . ' ' . $user['lastname'] ?></h2>
+
 <p>Email: <?= $user['email'] ?></p>
 <p>Age: <?= $user['age'] ?></p>
+
 <p><a href=<?= DOMAIN . 'usercontroller/edit/' . $user['id'] ?>>Edit</a></p>
+
 <form action=<?= DOMAIN . 'usercontroller/destroy/' . $user['id'] ?> method="POST">
     <input type="submit" value="Delete">
 </form>
