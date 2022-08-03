b0b9a39e57fe7d0ae0b801cb564dc627d5bcd282
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-03 14:52:50 -0300

    feat: add try-catch and show error message in case connection fails

--- app/libraries/Connection.php
+++ app/libraries/Connection.php
@@ -10,10 +10,16 @@
 
         protected function connect()
         {
-            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->database . ';charset=' . $this->charset;
-            $pdo = new PDO($dsn, $this->user, $this->password);
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
+            try
+            {
+                $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->database . ';charset=' . $this->charset;
+                $pdo = new PDO($dsn, $this->user, $this->password);
+                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $pdo;
+            } catch(PDOException $e)
+            {
+                echo 'Connection failed: ' . $e->getMessage();
+            }
         }
     }
