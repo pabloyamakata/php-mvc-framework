622102d8417bde78d692677d3e02b95340fe7d97
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-03 18:57:15 -0300

    refactor: require Config class

--- app/init.php
+++ app/init.php
@@ -1,7 +1,6 @@
 <?php
 
-    require_once 'config/database.php';
-    require_once 'config/location.php';
+    require_once 'config/Config.php';
     require_once 'helpers/redirect.php';
 
     spl_autoload_register(fn($class) => require_once 'libraries/' . $class . '.php');
