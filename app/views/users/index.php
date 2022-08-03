dc869fcb220bd2eaba1fdd6f5868e522810dc00e
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-02 21:43:30 -0300

    feat: create html structure that displays all records of a table

---
+++ app/views/users/index.php
@@ -0,0 +1,34 @@
+<?php include_once ROOT_PATH . 'app/views/inc/header.php'; ?>
+
+<table>
+    <thead>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+        <?php
+        
+            foreach($users as $user)
+            {
+        
+        ?>
+
+        <tr>
+            <td><?= $user['firstname'] ?></td>
+            <td><?= $user['lastname'] ?></td>
+            <td><?= $user['email'] ?></td>
+            <td><?= $user['age'] ?></td>
+            <td><a href=<?= DOMAIN . 'usercontroller/show/' . $user['id'] ?>>Show</a></td>
+        </tr>
+
+        <?php } ?>
+
+    </tbody>
+</table>
+
+<?php include_once ROOT_PATH . 'app/views/inc/footer.php'; ?>
