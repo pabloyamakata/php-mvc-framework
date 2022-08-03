52d81f761bf6c0f11673babc9cfc5f2b7c31e717
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-03 14:43:18 -0300

    feat: set database charset to utf8mb4

--- app/libraries/Connection.php
+++ app/libraries/Connection.php
@@ -6,10 +6,11 @@
         private $user = DB_USER;
         private $password = DB_PASSWORD;
         private $database = DB_NAME;
+        private $charset = DB_CHARSET;
 
         protected function connect()
         {
-            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->database;
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->database . ';charset=' . $this->charset;
             $pdo = new PDO($dsn, $this->user, $this->password);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
