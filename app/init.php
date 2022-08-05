25f58a5a38ef0228b2c938f6efcccc5efbf94e5b
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-04 22:39:27 -0300

    refactor: require once Helper.php

--- app/init.php
+++ app/init.php
@@ -1,6 +1,6 @@
 <?php
 
     require_once 'config/Config.php';
-    require_once 'helpers/redirect.php';
+    require_once 'helpers/Helper.php';
 
     spl_autoload_register(fn($class) => require_once 'libraries/' . $class . '.php');
