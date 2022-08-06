701f85f6c480780745a5d8341f11244b881e77fc
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-05 22:32:55 -0300

    refactor: bring domain and rootDir static functions into this file

--- app/helpers/Helper.php
+++ app/helpers/Helper.php
@@ -2,8 +2,20 @@
 
     class Helper
     {
+        public static function domain()
+        {
+            $domain = 'http://localhost/php-mvc-framework/public/';
+            return $domain;
+        }
+
+        public static function rootDir()
+        {
+            $rootDir = str_replace('\\', '/', dirname(dirname(__DIR__)) . '/');
+            return $rootDir;
+        }
+
         public static function redirect($path)
         {
-            header('Location: ' . Config::domain() . $path);
+            header('Location: ' . self::domain() . $path);
         }
     }
