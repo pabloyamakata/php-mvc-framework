d897dee9ff62e2b2acba0c9044029069a7ce0281
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-06 00:29:51 -0300

    style: add indentation and line breaks

--- app/views/users/show.php
+++ app/views/users/show.php
@@ -1,12 +1,13 @@
 <?php include_once Helper::rootDir() . 'app/views/inc/header.php'; ?>
 
     <h2><?= $user['firstname'] . ' ' . $user['lastname'] ?></h2>
-    
+
     <p>Email: <?= $user['email'] ?></p>
+    
     <p>Age: <?= $user['age'] ?></p>
-    
+
     <p><a href=<?= Helper::domain() . 'usercontroller/edit/' . $user['id'] ?>>Edit</a></p>
-    
+
     <form action=<?= Helper::domain() . 'usercontroller/destroy/' . $user['id'] ?> method="POST">
         <input type="submit" value="Delete">
     </form>
