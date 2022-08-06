049fe8a88a02fa94fc1a7cf17d0843ecd1232252
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-05 22:29:11 -0300

    feat: change access modifier to protected

--- app/config/Config.php
+++ app/config/Config.php
@@ -2,21 +2,9 @@
 
     class Config
     {
-        const DB_HOST = 'localhost';
-        const DB_USER = 'root';
-        const DB_PASSWORD = '';
-        const DB_NAME = 'websavant';
-        const DB_CHARSET = 'utf8mb4';
-
-        public static function domain()
-        {
-            $domain = 'http://localhost/php-mvc-framework/public/';
-            return $domain;
-        }
-
-        public static function rootDirectory()
-        {
-            $rootDirectory = str_replace('\\', '/', dirname(dirname(__DIR__)) . '/');
-            return $rootDirectory;
-        }
+        protected const DB_HOST = 'localhost';
+        protected const DB_USER = 'root';
+        protected const DB_PASSWORD = '';
+        protected const DB_NAME = 'websavant';
+        protected const DB_CHARSET = 'utf8mb4';
     }
