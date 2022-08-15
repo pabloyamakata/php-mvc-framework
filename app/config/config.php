bc0ab139a985a2d8aea97abaf091177fa19fb1fd
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-14 21:41:25 -0300

    refactor: create file which contains app constants

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Database credentials
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'websavant');
+    define('DB_CHARSET', 'utf8mb4');
+
+    // Application's URL
+    define('APP_URL', 'http://localhost/php-mvc-framework/public/');
+
+    // Path to app directory
+    define('APP_DIR', str_replace('\\', '/', dirname(__DIR__) . '/'));
