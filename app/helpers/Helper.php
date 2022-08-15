2c762bbb184262e898ac193469dbbd4d67c79b8e
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-14 21:44:03 -0300

    refactor: remove helper functions

--- app/helpers/Helper.php
+++ app/helpers/Helper.php
@@ -2,20 +2,8 @@
 
     class Helper
     {
-        public static function domain()
-        {
-            $domain = 'http://localhost/php-mvc-framework/public/';
-            return $domain;
-        }
-
-        public static function rootDir()
-        {
-            $rootDir = str_replace('\\', '/', dirname(dirname(__DIR__)) . '/');
-            return $rootDir;
-        }
-
         public static function redirect($path)
         {
-            header('Location: ' . self::domain() . $path);
+            header('Location: ' . APP_URL . $path);
         }
     }
