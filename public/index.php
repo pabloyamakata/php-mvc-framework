995e79516d3b722a9ce9ee0fb3773ba2cc8033b3
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-03 00:10:22 -0300

    define routes in routes.php

--- public/index.php
+++ public/index.php
@@ -1,19 +1,7 @@
 <?php
 
     use App\Libraries\Router;
-    use App\Controllers\HomeController;
-    use App\Controllers\UserController;
 
     require_once '../app/init.php';
-
-    Router::get('/', [HomeController::class, 'index']);
-    
-    Router::get('/users', [UserController::class, 'index']);
-    Router::get('/users/create', [UserController::class, 'create']);
-    Router::post('/users/store', [UserController::class, 'store']);
-    Router::get('/users/{id}', [UserController::class, 'show']);
-    Router::get('/users/{id}/edit', [UserController::class, 'edit']);
-    Router::post('/users/{id}/update', [UserController::class, 'update']);
-    Router::post('/users/{id}/destroy', [UserController::class, 'destroy']);
 
     Router::run();
