c0a5196eb2066029829ce2bdd4008688cc96c1e7
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-02 23:59:39 -0300

    define app routes

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -1,3 +1,15 @@
 <?php
 
-// Routes should be defined in this file (pending)
+    use App\Libraries\Router;
+    use App\Controllers\HomeController;
+    use App\Controllers\UserController;
+
+    Router::get('/', [HomeController::class, 'index']);
+    
+    Router::get('/users', [UserController::class, 'index']);
+    Router::get('/users/create', [UserController::class, 'create']);
+    Router::post('/users/store', [UserController::class, 'store']);
+    Router::get('/users/{id}', [UserController::class, 'show']);
+    Router::get('/users/{id}/edit', [UserController::class, 'edit']);
+    Router::post('/users/{id}/update', [UserController::class, 'update']);
+    Router::post('/users/{id}/destroy', [UserController::class, 'destroy']);
