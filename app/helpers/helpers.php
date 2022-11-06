7c70b9661965dde6e86d592c17f0b9efe42c7700
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-05 21:52:15 -0300

    create helper functions to handle session in views

---
+++ app/helpers/helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+    function session()
+    {
+        if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+
+        return new class {
+            
+            public function has($title)
+            {
+                if(isset($_SESSION[$title])) return true;
+                return false;
+            }
+
+            public function get($title)
+            {
+                $sessionVar = $_SESSION[$title];
+                unset($_SESSION[$title]);
+
+                return $sessionVar;
+            }
+
+            public function hasError($field)
+            {
+                if(isset($_SESSION['errors'][$field])) return true;
+                return false;
+            }
+
+            public function getError($field)
+            {
+                $sessionVar = $_SESSION['errors'][$field][0];
+                unset($_SESSION['errors'][$field]);
+
+                return $sessionVar;
+            }
+        };
+    }
