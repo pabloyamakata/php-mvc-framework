3e48c8ad64670dbae0001d6238dd70b0a28e3bc5
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-05 21:47:10 -0300

    add methods to create session vars when redirecting from controller

--- app/helpers/Helper.php
+++ app/helpers/Helper.php
@@ -2,10 +2,24 @@
 
     namespace App\Helpers;
 
+    session_start();
+
     class Helper
     {
         public static function redirect($path)
         {
             header('Location: ' . APP_URL . $path);
+
+            return new self;
+        }
+
+        public function with($title, $content)
+        {
+            $_SESSION[$title] = $content;
+        }
+
+        public function withErrors($errors)
+        {
+            $_SESSION['errors'] = $errors;
         }
     }
