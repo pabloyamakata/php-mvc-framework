05893b94c4fd4218573e92ec9cced3544016026d
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-11-05 21:39:39 -0300

    show session messages

--- app/views/users/create.php
+++ app/views/users/create.php
@@ -2,20 +2,38 @@
 
     <form action=<?= APP_URL . 'users/store' ?> method="POST">
         
-        <label for="firstname">First Name</label>
-        <input type="text" id="firstname" name="firstname">
+        <div>
+            <label for="firstname">First Name</label>
+            <input type="text" id="firstname" name="firstname">
+        </div>
+
+        <?php if(session()->hasError('firstname')) echo session()->getError('firstname'); ?>
     
-        <label for="lastname">Last Name</label>
-        <input type="text" id="lastname" name="lastname">
+        <div>
+            <label for="lastname">Last Name</label>
+            <input type="text" id="lastname" name="lastname">
+        </div>
+
+        <?php if(session()->hasError('lastname')) echo session()->getError('lastname'); ?>
     
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email">
+        <div>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email">
+        </div>
+
+        <?php if(session()->hasError('email')) echo session()->getError('email'); ?>
     
-        <label for="age">Age</label>
-        <input type="number" id="age" name="age">
+        <div>
+            <label for="age">Age</label>
+            <input type="number" id="age" name="age">
+        </div>
+
+        <?php if(session()->hasError('age')) echo session()->getError('age'); ?>
     
         <input type="submit" value="Create User">
     
     </form>
+
+    <?php if(session()->has('success')) echo session()->get('success'); ?>
 
 <?php include_once APP_DIR . 'views/inc/footer.php'; ?>
